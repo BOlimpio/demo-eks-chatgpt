7da6a48660671c97974608fe4e20913304e46c74
BOlimpio <b.olimpio@outlook.com>
2023-02-15 23:56:03 -0300

    fix eks module usage

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,33 +12,53 @@ module "vpc" {
   tags = var.tags
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+module "eks_managed_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
 
+  name            = "separate-eks-mng"
   cluster_name    = var.cluster_name
+  cluster_version = "1.24"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+  // The following variables are necessary if you decide to use the module outside of the parent EKS module context.
+  // Without it, the security groups of the nodes are empty and thus won't join the cluster.
+  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
+  vpc_security_group_ids            = [module.eks.node_security_group_id]
 
+  // Note: `disk_size`, and `remote_access` can only be set when using the EKS managed node group default launch template
+  // This module defaults to providing a custom launch template to allow for custom security groups, tag propagation, etc.
+  // use_custom_launch_template = false
+  // disk_size = 50
+  //
+  //  # Remote access cannot be specified with a launch template
+  //  remote_access = {
+  //    ec2_ssh_key               = module.key_pair.key_pair_name
+  //    source_security_group_ids = [aws_security_group.remote_access.id]
+  //  }
+
+  min_size     = 1
+  max_size     = 4
+  desired_size = 2
+
+  instance_types = ["t3.small"]
+  capacity_type  = "SPOT"
+
+  labels = {
+    Environment = "test"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
   }
 
-  eks_managed_node_groups = {
-    one = {
-      name = "chatgpt-ng-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+  taints = {
+    dedicated = {
+      key    = "dedicated"
+      value  = "gpuGroup"
+      effect = "NO_SCHEDULE"
     }
   }
 
-  depends_on = [module.vpc]
+  tags = var.tags
 }
 
 # module "eks" {
