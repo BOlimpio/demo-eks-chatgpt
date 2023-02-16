8fb08a5c903fdf72722c750434ee68233d11fc95
BOlimpio <b.olimpio@outlook.com>
2023-02-15 23:45:52 -0300

    fix eks module usage

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,6 +12,34 @@ module "vpc" {
   tags = var.tags
 }
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = var.cluster_name
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "chatgpt-ng-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+  
+  depends_on = [module.vpc]
+}
 
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
