f255aa929ae25339e874019a5e96e157fc01c559
BOlimpio <b.olimpio@outlook.com>
2023-02-22 23:06:03 -0300

    add eks blueprints module instead of eks module

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,77 +12,40 @@ module "vpc" {
   tags = var.tags
 }
 
-module "eks_managed_node_group" {
-  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+//ToDo USE EKS BLUEPRINTS
 
-  name            = "separate-eks-mng"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.24"
+module "eks_blueprints" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints"
 
-  subnet_ids = module.vpc.private_subnets
+  # EKS CLUSTER
+  cluster_version    = "1.24"                                         # EKS Cluster Version
+  vpc_id             = module.vpc.vpc_id                              # Enter VPC ID
+  private_subnet_ids = module.vpc.private_subnets                     # Enter Private Subnet IDs
 
-  // The following variables are necessary if you decide to use the module outside of the parent EKS module context.
-  // Without it, the security groups of the nodes are empty and thus won't join the cluster.
-  cluster_primary_security_group_id = module.eks.cluster_primary_security_group_id
-  vpc_security_group_ids            = [module.eks.node_security_group_id]
+  # EKS Addons
+  enable_amazon_eks_vpc_cni = true
+  enable_amazon_eks_coredns = true
+  enable_amazon_eks_kube_proxy = true
 
-  // Note: `disk_size`, and `remote_access` can only be set when using the EKS managed node group default launch template
-  // This module defaults to providing a custom launch template to allow for custom security groups, tag propagation, etc.
-  // use_custom_launch_template = false
-  // disk_size = 50
-  //
-  //  # Remote access cannot be specified with a launch template
-  //  remote_access = {
-  //    ec2_ssh_key               = module.key_pair.key_pair_name
-  //    source_security_group_ids = [aws_security_group.remote_access.id]
-  //  }
+  # EKS MANAGED NODE GROUPS
+  managed_node_groups = {
+    mng = {
+      node_group_name = "chatgpt-ng-1"
+      instance_types  = ["t3.medium"]
+      subnet_ids      = module.vpc.private_subnets  # Mandatory Public or Private Subnet IDs
+      disk_size       = 100 # disk_size will be ignored when using Launch Templates
+      capacity_type   = "ON_DEMAND"  # ON_DEMAND or SPOT
+      ami_type        = "AL2_x86_64" # Amazon Linux 2(AL2_x86_64), AL2_x86_64_GPU, AL2_ARM_64, BOTTLEROCKET_x86_64, BOTTLEROCKET_ARM_64
 
-  min_size     = 1
-  max_size     = 4
-  desired_size = 2
-
-  instance_types = ["t3.small"]
-  capacity_type  = "SPOT"
-
-  labels = {
-    Environment = "test"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
-  }
-
-  taints = {
-    dedicated = {
-      key    = "dedicated"
-      value  = "gpuGroup"
-      effect = "NO_SCHEDULE"
+      # Node Group scaling configuration
+      desired_size    = 2
+      max_size        = 3
+      min_size        = 1
     }
   }
 
-  tags = var.tags
+  depends_on = [module.vpc]
 }
-
-# module "eks" {
-#   source = "terraform-aws-modules/eks/aws"
-
-#   cluster_name = var.cluster_name
-#   subnet_ids = module.vpc.private_subnets
-#   vpc_id = module.vpc.vpc_id
-#   cluster_endpoint_public_access = true
-#   tags = var.tags
-#   eks_managed_node_groups = {
-#     one = {
-#       name = "chatgpt-ng-1"
-
-#       instance_types = ["t3.small"]
-
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 2
-#     }
-#   }
-
-#   depends_on = [module.vpc]
-# }
 
 # Deploy Prometheus using Helm
 resource "helm_release" "prometheus" {
@@ -94,6 +57,11 @@ resource "helm_release" "prometheus" {
   # Set the values for the Prometheus Helm chart
   values = [
     file("${path.module}/prometheus.yaml"),
+  ]
+
+  depends_on = [
+    module.eks_blueprints,
+    module.vpc,
   ]
 }
 
@@ -112,5 +80,7 @@ resource "helm_release" "grafana" {
   # Add a dependency on the Prometheus deployment
   depends_on = [
     helm_release.prometheus,
+    module.eks_blueprints,
+    module.vpc,
   ]
 }
